9bf7e9e8c5534c04350f09cef726b28cc01c80eb
Dmitry Kuzin <dmitry@kuzin.es>
2018-11-29 13:48:36 +0300

    Initial Commit

---
+++ SkogenCoinAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkogenCoinAdmin
+//
+//  Created by Dmitry Kuzin on 29/11/2018.
+//  Copyright © 2018 SkogenCoin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
